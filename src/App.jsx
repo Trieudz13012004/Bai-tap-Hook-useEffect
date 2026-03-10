a527f839efedb1bb8b6df3ba8a8f1d695cde9a5a
Ha Trieu <Havantrieu1301204@gmail.com>
2026-03-10 20:57:36 +0700

    Bài tập Hook và useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  // const [count, setCount] = useState(0)
+export default function App() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    try {
+      // Try catch ko bắt lỗi của fetch .then()
+      fetch("https://dogapi.dog/api/v2/breeds")
+        .then((res) => res.json())
+        .then((dogs) => setData(dogs.data))
+        .finally(() => setIsLoading(false));
+    } catch (error) {
+      setError(error);
+    }
+  }, []);
+
+  if (error) return <p>{error}</p>;
+
+  if (isLoading) return <p>Loading....</p>;
 
   return (
-    <>
-      
-    </>
-  )
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(4, 1fr)",
+        justifyContent: "space-between",
+        gap: "30px",
+        padding: "0px 100px"
+      }}
+    >
+      {data.length === 0 ? (
+        <p>Không có dữ liệu</p>
+      ) : (
+        data.map((dog) => <DogItem key={dog.id} {...dog} />)
+      )}
+    </div>
+  );
 }
 
-export default App
+const DogItem = ({ id, type, attributes }) => {
+  return (
+    <div style={{ width: "298px", height: "350px", justifyContent: "center"}}>{attributes.name}</div>
+  );
+};
